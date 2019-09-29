2965e2e8397fa8a4f2f59c381349c7cee1c5806c
SteveMckoyII <55262032+SteveMckoyII@users.noreply.github.com>
2019-09-28 20:02:59 -0500

    Add files via upload
    Kinda broken but working product

--- main.py
+++ main.py
@@ -18,3 +18,14 @@ def convert_image(input_file, output_file):
 	pixel_array = np.array(img)
 	#assigns characters to pixel values
 	ASCII = ['.',',','*','^','!','|','+','-',';','o','x','%','?','$','#','@']
+
+	f = open(output_file, 'w')
+
+	for x in pixel_array:
+		for y in x - 1:
+			f.write(ASCII[trunc(y/255*17)-1])
+		f.write('\n')
+	f.close()
+	return True
+
+convert_image('picture.png', 'picutre.text')
